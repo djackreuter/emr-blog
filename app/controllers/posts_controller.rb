304db036fdd089f0177c6ed810e8d8c0f75a0801
Jack Reuter <djreuter45@gmail.com>
2018-01-22 12:07:41 -0700

    added create, show, and index methods to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,30 @@
+# controller for posts
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order('created_at DESC')
+  end
 
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
-
+  
   end
 
   def create
-    
+    @post = Post.new(params[:post])
+
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
