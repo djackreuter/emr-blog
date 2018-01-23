acafbecd11914835c1c30583784047e2be385096
Jack Reuter <djreuter45@gmail.com>
2018-01-23 10:23:34 -0700

    defined associations and updated migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123171849) do
+ActiveRecord::Schema.define(version: 20180123172259) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
