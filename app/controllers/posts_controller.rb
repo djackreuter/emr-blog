77e83422ead5734c3290db387cfae7ae86b9b7c0
Jack Reuter <djreuter45@gmail.com>
2018-01-23 09:51:00 -0700

    added edit and update methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,10 @@ class PostsController < ApplicationController
     @post = current_user.posts.build
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = current_user.posts.build(post_params)
 
@@ -22,6 +26,15 @@ class PostsController < ApplicationController
       redirect_to root_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
 
