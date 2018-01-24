5abe2d8a295bb7c57b346a2b89096ada9e5d7588
Jack Reuter <djreuter45@gmail.com>
2018-01-24 15:18:56 -0700

    added validates for ppost body

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
