e8813a011cb71e30570aee156d61f4799ad98c57
Jack Reuter <djreuter45@gmail.com>
2018-01-24 15:31:52 -0700

    wrote test for post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context 'validation tests' do
+    it 'ensures title presence' do
+      post = Post.new(body: 'this is a test body').save
+      expect(post).to eq(false)
+    end
+
+    it 'ensures body presence' do
+      post = Post.new(title: 'this is a text title').save
+      expect(post).to eq(false)
+    end
+
+    it 'ensures min title length is met' do
+      post = Post.new(title: 'te', body: 'this is a test body').save
+      expect(post).to eq(false)
+    end
+
+    it 'ensures min body length is met' do
+      post = Post.new(title: 'this is a test title', body: 'this is').save
+      expect(post).to eq(false)
+    end
+
+    it 'should save successfully' do
+      user = User.new
+      user.save(validate: false)
+      post = Post.new(title: 'this is a test title', body: 'this is a test body', user: user).save
+      expect(post).to eq(true)
+    end
+  end
+end
