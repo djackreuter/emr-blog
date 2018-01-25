1429a41f7842c93a59d3fcefe9437ff6e5b3d152
Jack Reuter <djreuter45@gmail.com>
2018-01-25 12:52:54 -0700

    added tests for template rendering

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -14,34 +14,60 @@ RSpec.describe PostsController, type: :controller do
     post
   end
 
-  context 'GET #index' do
+  context 'GET #index and show index view' do
     it 'returns a success response' do
       get :index
       expect(response).to be_success
     end
+
+    it 'renders the index view' do
+      get :index
+      expect(response).to render_template(:index)
+    end
   end
 
-  context 'GET #show' do
+  context 'GET #show and render show view' do
     it 'returns a success response' do
       new_post
       get :show, params: { id: new_post.id }
       expect(response).to be_success
     end
-  end
 
-  context 'GET #new' do
-    it 'returns a success response' do
+    it 'renders show view' do
       new_post
-      expect(response).to be_success
+      get :show, params: { id: new_post.id }
+      expect(response).to render_template(:show)
     end
   end
 
-  context 'GET #edit' do
+  context 'GET #new and render new template' do
+    it 'returns a success response' do
+      user
+      get :new
+      new_post
+      expect(response).to be_success
+    end
+
+    it 'renders new template' do
+      user
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  context 'GET #edit and render edit template' do
     it 'returns a success response' do
       new_post
       user
       get :edit, params: { id: new_post.id }
       expect(response).to be_success
+    end
+
+    it 'renders edit template' do
+      user
+      new_post
+      get :edit, params: { id: new_post.id }
+      expect(response).to render_template(:edit)
     end
   end
 
