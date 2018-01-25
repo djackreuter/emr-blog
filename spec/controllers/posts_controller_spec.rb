e80072a0e5c19f785c248e10f2605f4f0ab6430f
Jack Reuter <djreuter45@gmail.com>
2018-01-25 12:00:01 -0700

    refactored and cleaned up code.  Also added method to test create

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 require 'faker'
 
 RSpec.describe PostsController, type: :controller do
+  let(:user) do
+    user = User.new(name: 'Johnny Vincint', email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+    user.save(validate: false)
+    sign_in user
+  end
+
+  let(:new_post) do
+    post = Post.new(title: 'Test title', body: 'test post body', user_id: 1)
+    post.save(validate: false)
+    post
+  end
+
   context 'GET #index' do
     it 'returns a success response' do
       get :index
@@ -11,30 +23,40 @@ RSpec.describe PostsController, type: :controller do
 
   context 'GET #show' do
     it 'returns a success response' do
-      post = Post.new(title: 'Test title', body: 'test post body', user_id: 1)
-      post.save(validate: false)
-      get :show, params: { id: post.id }
+      new_post
+      get :show, params: { id: new_post.id }
       expect(response).to be_success
     end
   end
 
   context 'GET #new' do
     it 'returns a success response' do
-      post = Post.new
-      post.save(validate: false)
+      new_post
       expect(response).to be_success
     end
   end
 
   context 'GET #edit' do
     it 'returns a success response' do
-      user = User.new(name: 'Johnny Vincint', email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
-      user.save(validate: false)
-      sign_in user
-      post = Post.new(title: 'Test title', body: 'test post body', user_id: 1)
-      post.save(validate: false)
-      get :edit, params: { id: post.id }
+      new_post
+      user
+      get :edit, params: { id: new_post.id }
       expect(response).to be_success
+    end
+  end
+
+  context 'POST #create' do
+    it 'redirects to root path' do
+      new_post
+      user
+      post :create, params: { post: { title: new_post.title, body: new_post.body } }
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  context 'PATCH #update' do
+    it 'redirects to post_path' do
+
     end
   end
 end
