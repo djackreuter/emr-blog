234beff9a6adf94b51b45f3bb4441814538d1658
Jack Reuter <djreuter45@gmail.com>
2018-01-30 15:58:54 -0700

    adding first post mailer job

---
+++ app/jobs/first_post_mailer_job.rb
@@ -0,0 +1,26 @@
+class FirstPostMailerJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(user)
+    ApplicationMailer.first_post_notification(user)
+  end
+end
+
+# class FirstPostMailer < Struct.new(:user)
+#   attr_accessor :user_id
+
+#   def deliver
+#     ApplicationMailer.first_post_notification(@user).deliver_now
+#   end
+
+#   def self.delay(user_id)
+#     f = FirstPostMailer.new
+#     f.user_id = user_id
+#     f
+#   end
+
+#   def perform
+#     @user = User.find(self.user_id)
+#     deliver
+#   end
+# end
