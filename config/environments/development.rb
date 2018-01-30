eb31cdf60ce8d308b1bc45674d11ab1dcc2d2345
Jack Reuter <djreuter45@gmail.com>
2018-01-30 11:27:22 -0700

    added more configurations for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,10 +29,12 @@ Rails.application.configure do
   # care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  Rails.application.routes.default_url_options[:host] = ENV['MAIL_HOST']
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.mailgun.org',
