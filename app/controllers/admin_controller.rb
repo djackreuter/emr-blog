5cc06bba851bb33f3aa7f3f17772478028a36065
Jack Reuter <djreuter45@gmail.com>
2018-01-31 14:40:35 -0700

    added admin controller

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,19 @@
+class AdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_index_path
+  end
+
+  private
+
+  def require_admin
+    redirect_to root_path unless current_user.admin?
+  end
+end
