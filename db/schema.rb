353e6ca207f0ed410cee084746a54ff1bd23b03d
Jack Reuter <djreuter45@gmail.com>
2018-01-31 13:38:33 -0700

    updated migration with default value

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20180131192323) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
