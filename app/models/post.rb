9377be39b4418c355490f67bc88c806f5b213fbb
Jack Reuter <djreuter45@gmail.com>
2018-02-01 15:25:12 -0700

    added inverse_of and presence validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user, inverse_of: :posts
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { minimum: 10 }
+  validates_presence_of :user
 end
