63ad05245cb0fe436f8cd68a243fb4b987e5a1ba
Jack Reuter <djreuter45@gmail.com>
2018-02-01 14:14:25 -0700

    added optional to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { minimum: 10 }
