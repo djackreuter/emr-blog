920ba8cc0c1af28bcee706b4c5f6ddb4a976c99c
Jack Reuter <djreuter45@gmail.com>
2018-02-01 15:58:17 -0700

    modifing devise registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,19 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout 'application'
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  before_action :sign_up_params
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    build_resourse({})
+    resource.build_posts
+    respond_with self.resource
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -36,7 +39,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  private
+
+  def sign_up_params
+    params.require(resource_name).permit(:name, :email, :password, :password_confirmation, posts_attributes: [:title, :body])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
