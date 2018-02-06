36432562e2f9c974e2df8c97ff79f7e26476f7c9
Jack Reuter <djreuter45@gmail.com>
2018-02-06 11:42:15 -0700

    added routes and renamed controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :admin, only: [:index, :destroy]
+  namespace 'api' do
+    namespace 'v1' do
+      resources :posts
+    end
+  end
 end
