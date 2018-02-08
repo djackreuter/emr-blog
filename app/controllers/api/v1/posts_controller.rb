7194718da15a65d531543449baaa9feeeda69bb0
Jack Reuter <djreuter45@gmail.com>
2018-02-08 13:23:55 -0700

    started api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,8 +2,19 @@ module Api
   module V1
     class PostsController < ApplicationController
       def index
-        posts = Post.order('created_at DESC')
+        if params[:created_at].present?
+          posts = Post.where(created_at: Time.zone.parse(params[:created_at]).at_beginning_of_day..Time.zone.parse(params[:created_at]).end_of_day)
+        elsif params[:start].present?
+          posts = Post.where(created_at: Time.zone.parse(params[:created_at]))
+        else
+          posts = Post.order('created_at DESC')
+        end
         render json: { status: 'SUCCESS', data: posts }, status: :ok
+      end
+
+      def show
+        post = Post.find(params[:id])
+        render json: { status: 'SUCCESS', data: post }, status: :ok
       end
     end
   end
