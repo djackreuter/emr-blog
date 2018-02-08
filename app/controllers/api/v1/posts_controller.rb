7f2563de2419527d4bb9ca265a8f8a0fe460d8ff
Jack Reuter <djreuter45@gmail.com>
2018-02-08 14:59:26 -0700

    added query for date range

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,7 +5,7 @@ module Api
         if params[:created_at].present?
           posts = Post.where(created_at: Time.zone.parse(params[:created_at]).at_beginning_of_day..Time.zone.parse(params[:created_at]).end_of_day)
         elsif params[:start].present?
-          posts = Post.where(created_at: Time.zone.parse(params[:created_at]))
+          posts = Post.where(created_at: Time.zone.parse(params[:start]).at_beginning_of_day..Time.zone.parse(params[:end]).end_of_day)
         else
           posts = Post.order('created_at DESC')
         end
