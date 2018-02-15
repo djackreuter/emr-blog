e8e4553ca28fb4f044223a5669ebb98ad0db4ba2
Jack Reuter <djreuter45@gmail.com>
2018-02-15 16:42:44 -0700

    added helper methods for users and posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def belongs_to_user?(post)
+    current_user.id == post.user_id
+  end
+
+  def admin?(post)
+    current_user.admin? && current_user.id != post.user_id
+  end
 end
