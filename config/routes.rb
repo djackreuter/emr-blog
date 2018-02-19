d7eadf3567940bcd04c79bbf14d329447042a23a
Jack Reuter <djreuter45@gmail.com>
2018-02-19 11:29:29 -0700

    added custom sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   resources :posts do
     resources :comments
   end
