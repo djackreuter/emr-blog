971d89814784d676c3cb09040e8f237af0db0843
Jack Reuter <djreuter45@gmail.com>
2018-02-20 14:05:14 -0700

    modified create method and added new methods for 2fa

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,14 +2,28 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
+
+  def verify
+  end
+
+  def check_2fa
+    user = User.find_by(session[:pre_2fa_id])
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_by(email: params.dig(:user, :email))
+    if user && user.valid_password?(params.dig(:user, :password))
+      session[:pre_2fa_id] = user.id
+      Authy::API.request_sms(id: user.authy_id)
+      redirect_to users_verify_path
+    else
+      new
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -23,3 +37,6 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 end
+# self.resource = warden.authenticate!(auth_options)
+# set_flash_message!(:notice, :signed_in)
+#     sign_in(resource_name, resource)
