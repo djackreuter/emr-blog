dd351d1b7e28f1e546ccd10780bc70cb834c3be6
Jack Reuter <djreuter45@gmail.com>
2018-02-20 11:57:13 -0700

    added routes for 2fa view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_scope :user do
+    get '/users/verify', to: 'users/sessions#verify'
+  end
   resources :posts do
     resources :comments
   end
